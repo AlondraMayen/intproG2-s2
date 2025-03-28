17e446d23e755af96c0d69e22e4f8407e2439a4c
Alondra <asmayen@uamv.edu.ni>
2025-03-28 08:25:20 -0600

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+nombre= input ("Dime tu nombre: ")
+edad = int("Dime tu edad: ")
+peso = float (input("Dime tu peso"))
+edad_dias = edad * 365
+peso_kg = peso / 2.204
+print("Estimad@", nombre, "tu edad en dias es:", edad_dias, "tu peso en kg es:", peso_kg)
